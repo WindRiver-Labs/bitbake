389564fbb4c3e29b2cd3e669005dde10783a78e0
Jason Wessel <jason.wessel@windriver.com>
2012-06-05 08:24:01 -0500

    knotty.py: update footer on dynamic log level change
    This patch adds a call to update the task list after changing log
    modes dynamically such that you can immediately see the remaining
    tasks.
    
    (LOCAL REV: NOT UPSTREAM) -- Sent to bitbake-devel on 20120917
    
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>
    
    Issue: LIN1019-1168
    Signed-off-by: Hongxu Jia <hongxu.jia@windriver.com>
    [MAA: context updates after upstream moved to py logging]
    Signed-off-by: Mark Asselstine <mark.asselstine@windriver.com>

--- lib/bb/ui/knotty.py
+++ lib/bb/ui/knotty.py
@@ -259,6 +259,10 @@ class TerminalFilter(object):
             print("Bitbake still alive (%ds)" % t)
             sys.stdout.flush()
 
+    def updateFooterForce(self):
+        self.footer_present = False
+        self.updateFooter()
+
     def updateFooter(self):
         if not self.cuu:
             return
@@ -659,6 +663,7 @@ def main(server, eventHandler, params, tf = TerminalFilter):
                 if termfilter.poll():
                     keyinput = sys.stdin.read(1)
                     rtloglevel.setLevel(keyinput, True)
+                    termfilter.updateFooterForce()
                 # Always try printing any accumulated log files first
                 rtloglevel.displayLogs()
                 if event is None:
