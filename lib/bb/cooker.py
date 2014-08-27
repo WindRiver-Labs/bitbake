9a0a1f407712e6e8417a6292a9b9f7291288c4ce
Hongxu Jia <hongxu.jia@windriver.com>
2014-08-27 11:32:35 +0800

    bitbake: runqueue/cooker: catch hashvalidate error in RunQueueExecuteScenequeue
    We need to catch the failure of self.rq.hashvalidate which invoked
    in RunQueueExecuteScenequeue, and exit in a normal build. So we
    raise bb.BBHandledException for this specific error.
    
    Tweak execute_runqueue's exception process order. So the build will
    exit peacefully while the above failure occurs.
    
    (LOCAL REV: NOT UPSTREAM) -- Sent to bitbake-devel on 20140827
    
    Signed-off-by: Hongxu Jia <hongxu.jia@windriver.com>

--- lib/bb/cooker.py
+++ lib/bb/cooker.py
@@ -1435,7 +1435,7 @@ class BBCooker:
             except runqueue.TaskFailure as exc:
                 failures += len(exc.args)
                 retval = False
-            except SystemExit as exc:
+            except (SystemExit, bb.BBHandledException) as exc:
                 self.command.finishAsyncCommand(str(exc))
                 return False
 
@@ -1471,7 +1471,7 @@ class BBCooker:
             except runqueue.TaskFailure as exc:
                 failures += len(exc.args)
                 retval = False
-            except SystemExit as exc:
+            except (SystemExit, bb.BBHandledException) as exc:
                 self.command.finishAsyncCommand(str(exc))
                 return False
 
